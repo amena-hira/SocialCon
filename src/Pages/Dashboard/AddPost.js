4f8b4c65ea72bb17fe7b22f5ff6676d47bb192c0
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-02-16 20:18:42 -0800

    multiple images and submit button enable after adding text and image

--- src/Pages/Dashboard/AddPost.js
+++ src/Pages/Dashboard/AddPost.js
@@ -1,9 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../Context/AuthProvider';
 import upload from '../../images/upload.svg';
 
 const AddPost = () => {
     const { user } = useContext(AuthContext);
+    const [messageValue, setMessageValue] = useState(false);
+    const [imagesValue, setImagesValue] = useState(false);
+    const [imageLength, setImageLength] = useState('');
+    const handleAddPost = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const postMessage = form.postMessage.value;
+        const images = form.images.files;
+        console.log(postMessage, images, 12);
+        form.reset();
+        setMessageValue('');
+        setImagesValue('');
+    }
     return (
         <div className=''>
             <div className="hero py-20">
@@ -14,27 +27,29 @@ const AddPost = () => {
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm lg:max-w-xl shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <form action="" className='flex justify-between'>
+                            <form onSubmit={handleAddPost} className='flex justify-between'>
                                 <div className="form-control">
-                                    <textarea className="textarea textarea-info" placeholder="Post Message"></textarea>
+                                    <textarea className="textarea textarea-info" name='postMessage' placeholder="Post Message" onChange={()=>{setMessageValue(true)}}></textarea>
                                 </div>
                                 <div className="form-control ">
                                     <label className="label">
                                         <span className="label-text">Images</span>
                                     </label>
                                     <label>
-                                        <input type="file" className='hidden' />
+                                        <input multiple type="file" name='images' className='hidden' onChange={(e)=>{setImagesValue(true);setImageLength(e.target.files)}}/>
                                         <img src={upload} className='h-6 w-6' alt="" />
+                                        {imageLength.length>0 && <label>{imageLength.length} Images</label>}
+                                        {imageLength.length === 0 && <span className='text-xs text-sky-900'>**Once select multiple</span>}
                                     </label>
                                     {
-                                        user?.uid?
-                                        <input type='submit' className="mt-3 btn mt-6 bg-sky-500 border-none hover:bg-pink-900 text-white max-w-xl lg:hidden" value="Submit" />
+                                        messageValue && imagesValue && user?.uid?
+                                        <input type='submit' className="mt-3 btn mt-6 bg-sky-500 border-none hover:bg-sky-900 text-white max-w-xl lg:hidden" value="Submit" />
                                         :
                                         <button type="button" className="mt-3 px-8 py-3 text-white bg-sky-300 rounded focus:outline-none max-w-xl lg:hidden" disabled>Submit</button>
                                     }
                                 </div>
                                 {
-                                    user?.uid ?
+                                    messageValue && imagesValue && user?.uid ?
                                         <div className="mt-6">
                                             <button className='btn bg-sky-500 border-none hover:bg-sky-900 text-white max-w-xl hidden lg:block'>Submit</button>
                                         </div>
